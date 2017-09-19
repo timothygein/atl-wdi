520cf2166f2ede14f3970143f80796da8ed04d3f
jamespeterson <jamespeterson0918@yahoo.com>
2017-09-19 19:51:10 -0400

    completed GET routes

--- homework/pizza-express/app.js
+++ homework/pizza-express/app.js
@@ -2,7 +2,24 @@
 var express = require('express');
 var app     = express();
 var port    = 3000;
+app.get('/greeting', function(req, res) {
+    console.log(req.query);
+    
+    res.render('greeting', {
+      data: req.query.saying
+    });
+});
 
+app.get('/topping/:pepperoni', function(req, res, next) {
+    
+        res.send( // something );
+    });
+
+    app.get('/order/:amount/:size', function(req, res, next) {
+        var numOfPies = 10;
+        var sizeOfPie = medium;
+             res.send( // something );
+         });
 
 app.listen(port, function(){
   console.log("==========================")
