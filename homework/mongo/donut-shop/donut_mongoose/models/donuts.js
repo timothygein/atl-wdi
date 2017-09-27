e71b317681fb1eaa66ad2fff0f0704df4f12d579
jamespeterson <jamespeterson0918@yahoo.com>
2017-09-26 20:24:20 -0400

    successfully seeded Donut data

--- homework/mongo/donut-shop/donut_mongoose/models/donuts.js
+++ homework/mongo/donut-shop/donut_mongoose/models/donuts.js
@@ -1,7 +1,29 @@
 //requirements: require mongoose
-
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+const Schema = mongoose.Schema;
 
 //create your donut schema:
+var donutSchema = new Schema({
+    
+    });
 
+
+
+
+
+
+    const newDonut = new Schema ({
+            name: "Chocolate Donut",
+            description: "I am a chocolate donut.",
+            img: "http://cdn.phillymag.com/wp-content/uploads/2013/09/donut.png",
+            price: 5,
+            qty: 99
+        }
+    );
 
 //export your donut with module.exports()
+module.exports = {
+    StudentModel: StudentModel,
+    ProjectModel: ProjectModel
+};
