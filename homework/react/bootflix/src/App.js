ef84d5a2a18fa9e1778af1ec9921cc9722dd8ce5
jamespeterson <jamespeterson0918@yahoo.com>
2017-10-10 18:54:08 -0400

    got it going on

--- homework/react/bootflix/src/App.js
+++ homework/react/bootflix/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 import Header from './components/Header';
 import Search from './components/Search';
 import Movie from './components/Movie';
@@ -8,25 +9,54 @@ class App extends Component {
   constructor(){
     super();
     this.state = {
-      movie: example
+      title: '',
+      year: ''
     }
   }
 
   //Update these methods to make axios calls to OMDB and update this.state.movie with the response from the server
-  _searchByTitle = () => {
-    console.log("Search by Title");
-  }
+  _searchByTitle = (event) => {
+    event.preventDefault()
+    console.log('This is searchByTitle')
+    const title = event.target.title.value
+    axios.get(`http://www.omdbapi.com/?apikey=d31f1a94&t=${title}`, {
+        }).then((response) => {
+          console.log(response)
+            this.setState({ 
+              title: response.data.Title,
+              year: response.data.Year 
+            })
+        }).catch((error) => {
+            console.log(error)
+        })
+      
+    }
 
-  _searchById = () => {
-    console.log("Search by ID");
-  }
+  _searchById = (event) => {
+    axios.get('http://www.omdbapi.com/', {
+            params: {
+                apikey: 'd31f1a94'
+            }
+        }).then((response) => {
+            const _searchById = response.data.Id
 
+        }).catch((error) => {
+            console.log(error)
+        })
+      }
+
+  componentWillMount(){
+    this._searchById()
+  }
   //Pass _searchByTitle, _searchById, and this.state.movie to it's appropriate child components.
   render() {
+    console.log(this.state.title)
+    console.log(this.state.year)
+
     return (
       <div className="App">
         <Header />
-        <Search />
+        <Search searchByTitle={this._searchByTitle} searchById={this._searchById}/>
         <Movie />
       </div>
     );
